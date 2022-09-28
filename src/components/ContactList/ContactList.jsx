fe42421ae06168c03cebc230cdca5e40dd71f4a3
AnnaLuniushkina <anna.luniushkina@gmail.com>
2022-09-29 00:10:36 +0300

    add change

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
 import contactsOperations from 'redux/contacts/contacts-operations';
