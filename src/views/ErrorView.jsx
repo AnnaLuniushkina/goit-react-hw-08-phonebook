fc2438228b1c6f201cf0bfec27ba5088c016d1d2
AnnaLuniushkina <anna.luniushkina@gmail.com>
2022-11-23 01:00:24 +0200

    fixed

--- src/views/ErrorView.jsx
+++ src/views/ErrorView.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom"
+import s from './ErrorView.module.css';
 
 export default function ErrorView() {
     return (
         <>
-            <h1>404 Page not found</h1>
+            <h1 className={s.titleError}>404 Page not found</h1>
             <NavLink to="/">
-                <button type="button">Go Home Page</button>
+                <button className={s.errorButton} type="button">Go Home Page</button>
             </NavLink>
         </>
     );
